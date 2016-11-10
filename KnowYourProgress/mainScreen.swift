cee4bbeb9e948c1f101b30935de48c808e8c58cd
Horváth Richárd <ricsy7@gmail.com>
2016-11-10 21:42:03 +0100

    database kialakítva, sikeresen beolvassa

--- KnowYourProgress/mainScreen.swift
+++ KnowYourProgress/mainScreen.swift
@@ -9,12 +9,53 @@
 import Foundation
 import UIKit
 
-class main: UIViewController {
+class main: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var productArray = NSArray()
+    
+    func readPropertyList(){
+
+            let plistPath:String? = Bundle.main.path(forResource: "gazdinfoData", ofType: "plist")!
+        
+            productArray = NSArray(contentsOfFile: plistPath!)!
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        readPropertyList()
     }
+    
+
+    //MARK: - Table View Data sources and Delegates
+    private func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return productArray.count
+    }
+    private func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Tantárgyak"
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath)
+        let row = indexPath.row
+        cell.textLabel?.text = getNev(pId: row)
+        return cell
+        
+    }
+ 
+    func getNev(pId: Int) -> String{
+        var nev: String = ""
+        var rekord: Dictionary<String, AnyObject>
+        rekord = productArray.object(at: pId) as! Dictionary<String, AnyObject>
+        
+        nev = rekord["nev"] as! String
+        
+        return nev;
+    }
+    
     func openNewPage(name: String){
         let theDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
         
